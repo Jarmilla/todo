2fc9481d01cb90a83bd8ba560db3621b51b37b02
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-02-09 11:20:30 +0100

    Delete dashboards

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,30 +1,31 @@
+import React, { useState } from "react";
 import "./App.css";
 
-const defaultState = [
+const initDashboards = [
   {
     id: 1,
-    name: "Darren tasks",
+    title: "Darren tasks",
     todoList: [
-      { id: 1, name: "protect the priest", desc: "not let yourself deceive" },
-      { id: 2, name: "sleep after breakfast", desc: "be ready!" },
+      { id: 2, name: "protect the priest", desc: "not let yourself deceive" },
+      { id: 3, name: "sleep after breakfast", desc: "be ready!" },
     ],
   },
   {
-    id: 1,
-    name: "Luna tasks",
+    id: 4,
+    title: "Luna tasks",
     todoList: [
       {
-        id: 1,
+        id: 5,
         name: "go to all the host families",
         desc: "dont say yes to nobody",
       },
       {
-        id: 2,
+        id: 6,
         name: "go to date with James Blonde",
         desc: "a promise is a promise",
       },
       {
-        id: 3,
+        id: 7,
         name: "monsterhunters are still here?",
         desc: "also there is something wrong with the ground",
       },
@@ -33,22 +34,39 @@ const defaultState = [
 ];
 
 function App() {
+  const [dashboards, setDashboards] = useState(initDashboards);
+
+  const deleteDashboard = (id) => {
+    /*     let newDashboard = [];
+        for (let i = 0; i < dashboards.length; i++) {
+          let currentDashboard = dashboards[i];
+          if (currentDashboard.id !== id) newDashboard.push(currentDashboard);
+        }
+        setDashboards(newDashboard); */
+    const newDashboard = dashboards.filter((dashboard) => dashboard.id !== id);
+    setDashboards(newDashboard);
+
+    /* setDashboards(dashboards.filter((dashboard) => dashboard.id !== id)); */
+  };
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      {defaultState.map((dashboard) => (
-        <article className="dashboard" key={dashboard.id}>
-          <h2>{dashboard.name}</h2>
+      {dashboards.map((dashboard) => (
+        <div className="dashboard" key={dashboard.id}>
+          <h2>{dashboard.title}</h2>
+
           {dashboard.todoList.map((todo) => (
-            <div className="todo-cards">
+            <div className="todo-cards" key={todo.id}>
               <h3>{todo.name}</h3>
               <p>{todo.desc}</p>
             </div>
           ))}
-        </article>
+          <button onClick={() => deleteDashboard(dashboard.id)}> - </button>
+        </div>
       ))}
 
-      {/*       <div className="dashboard">
+      {/*<div className="dashboard">
         <h2>{defaultState[1].name}</h2>
         <div className="todo-cards">
           <h3>{defaultState[1].todoList[0].name}</h3>
